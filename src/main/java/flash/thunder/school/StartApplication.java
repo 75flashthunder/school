1723fa4ad434996649a4cc17989a027b877ba188
75flashthunder <1909520228@qq.com>
2018-09-25 14:59:18 +0800

    test

---
+++ src/main/java/flash/thunder/school/StartApplication.java
@@ -0,0 +1,16 @@
+package flash.thunder.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Administrator on 2018/8/7.
+ */
+@SpringBootApplication
+
+public class StartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class, args);
+    }
+}
+
