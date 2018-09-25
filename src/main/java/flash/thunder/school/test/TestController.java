d785dfc1a20cf9beb3d12f9e1c745dc19f504e3f
75flashthunder <1909520228@qq.com>
2018-09-25 16:44:00 +0800

    login.html

--- src/main/java/flash/thunder/school/test/TestController.java
+++ src/main/java/flash/thunder/school/test/TestController.java
@@ -3,14 +3,12 @@ package flash.thunder.school.test;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by Administrator on 2018/8/7.
  */
 @Controller
 public class TestController {
-    @ResponseBody
     @RequestMapping(value = "/helloworld", method = RequestMethod.GET, produces = "application/json")
     public String helloWorld(){
         return "helloWorld";
